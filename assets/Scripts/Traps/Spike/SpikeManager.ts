8c1c7fdd11afd78313dde68e1c395855a48d381f
DullSword <525649253@qq.com>
2024-08-16 02:32:14 +0800

    🐞 fix(spike): 现在 spike 会正确计数
    之前因为在播放最后一段动画也就是相当攻击动画时，可能会由于人物的移动，会播放下一段动画，导致原先的动画被打断，没能正确执行到 resetPoint 恢复点数，从而导致 spike 状态混乱。

--- assets/Scripts/Traps/Spike/SpikeManager.ts
+++ assets/Scripts/Traps/Spike/SpikeManager.ts
@@ -32,8 +32,13 @@ export class SpikeManager extends TrapManager {
             return;
         }
 
-        this.currentPoint++;
-        this.currentPoint = this.currentPoint % (this.totalPoint + 1);
+        // 正常应该在播放完最后一段动画后触发达 resetPoint，使 currentPoint 重置为 0，
+        // 但动画还没播放完时如果人物又进行移动执行到这里，那么就将 currentPoint 直接设置为 1
+        if (this.currentPoint === this.totalPoint) {
+            this.currentPoint = 1;
+        } else {
+            this.currentPoint++;
+        }
 
         if (this.currentPoint === this.totalPoint) {
             if (Vec2.distance(this.position, player.targetPosition) <= this.triggerDistance) {
